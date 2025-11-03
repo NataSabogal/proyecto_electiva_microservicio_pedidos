9532670c5878b9f16393f524069f370a722eb9f7
natasabogal <natalia.sabogal.1766@eam.edu.co>
2025-11-03 18:13:58 -0500

    creating tables in bd

--- src/main/java/com/farmacia/pedidos/PedidosServiceApplication.java
+++ src/main/java/com/farmacia/pedidos/PedidosServiceApplication.java
@@ -2,10 +2,15 @@ package com.farmacia.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.farmacia.pedidos", "controller", "service", "repository"})
+@EntityScan(basePackages = "entity")  // 👈 AGREGA ESTA LÍNEA
+@EnableJpaRepositories(basePackages = "repository")
+
 public class PedidosServiceApplication {
 
 	public static void main(String[] args) {
